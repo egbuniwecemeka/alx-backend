00f2edd6b9ced0724920f2da4aa6b987827e355f
egbuniwecemeka <egbuniwecemeka.com>
2024-10-29 14:30:49 +0100

    Simple helper function

--- 0x00-pagination/0-main.py
+++ 0x00-pagination/0-main.py
@@ -5,6 +5,6 @@ Main file
 
 index_range = __import__('0-simple_helper_function').index_range
 
-res = index_range(page=1, page_size=20)
+res = index_range(page=3, page_size=15)
 print(type(res))
 print(res)
