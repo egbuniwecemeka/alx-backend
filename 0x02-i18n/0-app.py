90976c3cc0e8edbf9a77177f8964d801fccd1595
egbuniwecemeka <egbuniwecemeka.com>
2024-11-05 14:07:36 +0100

    pycodestyle fix

--- 0x02-i18n/0-app.py
+++ 0x02-i18n/0-app.py
@@ -5,10 +5,11 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
 def hello():
-     """ Returns application Welcome Message """
-     return render_template('0-index.html')
+    """ Returns application Welcome Message """
+    return render_template('0-index.html')
 
 
 if __name__ == "__main__":
