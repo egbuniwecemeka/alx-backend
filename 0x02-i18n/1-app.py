b530c1a9fe28772d59ab42429783a4502e11ed10
egbuniwecemeka <egbuniwecemeka.com>
2024-11-05 17:15:13 +0100

    pycodestyle fix

--- 0x02-i18n/1-app.py
+++ 0x02-i18n/1-app.py
@@ -1,14 +1,16 @@
 #!/usr/bin/env python3
 """ A python script thats sets up and instantiates a basic Flask App"""
 
-from flask import Flask, render_template,g, request
+from flask import Flask, render_template, g, request
 from flask_babel import Babel
+
 
 class Config:
     """ Configures available languages for the application """
     LANGUAGES = ["en", "fr"]
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_DEFAULT_TIMEZONE = "UTC"
+
 
 app = Flask(__name__)
 app.config.from_object(Config)
@@ -19,16 +21,18 @@ babel = Babel(app)
 def hello():
     """ Returns the welcome message for my application """
     return render_template('0-index.html')
-    
+
+
 @babel.localeselector
 def get_locale():
-    """Get the best match from available languages based on user settings or request headers."""
+    """Get the best matched language based on user settings or request headers."""
     # use locale from user settings, if user is logged in
     user = getattr(g, 'user', None)
     if user is not None and hasattr(user, 'locale'):
         return user.locale
     # else guess the language from the user accept
     return request.accept_languages.best_match(Config.LANGUAGES)
+
 
 @babel.timezoneselector
 def get_timezone():
