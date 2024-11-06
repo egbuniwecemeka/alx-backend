16b2aadcf468b794e39256ac095e96427c90ec5b
egbuniwecemeka <egbuniwecemeka.com>
2024-11-06 14:31:26 +0100

    edited .po translation files

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -5,11 +5,6 @@ from flask import Flask, render_template
 from jinja2 import Environment, FileSystemLoader
 from flask_babel import gettext as _, Babel, request
 
-# Set up Jinja2 environment
-env = Environment(
-    loader=FileSystemLoader('templates'),
-    autoescape=True  # Enable autoescape for security
-)
 
 app = Flask(__name__)
 app.config['BABEL_DEFAULT_LOCALE'] = 'en'
