5e6d6e84222ca75ef6a49950ae402f531789ebc8
egbuniwecemeka <egbuniwecemeka.com>
2024-11-06 11:59:06 +0100

    pycodestyle edit

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -14,7 +14,7 @@ babel = Babel(app)
 @app.route("/", strict_slashes=False)
 def hello():
     """ returns the welcome messaage with translations """
-    return render_template('0-index.html')
+    return render_template('3-index.html')
 
 
 if __name__ == "__main__":
