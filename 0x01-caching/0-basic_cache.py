ed7519f80aa78cfae3dbffaffe10736ecf5789f0
egbuniwecemeka <egbuniwecemeka.com>
2024-11-24 21:11:41 +0100

    Basic dictionary

---
+++ 0x01-caching/0-basic_cache.py
@@ -0,0 +1,21 @@
+#!/usr/biin/env python3
+""" Creates a cache class that inherits from a a base cache"""
+
+BaseCaching = __import__('base_caching').BaseCaching
+
+def BasicCache(BaseCaching):
+    """A caching system"""
+    def put(self, key, item):
+        """Adds an item in the cache"""
+        if key is None or item is None:
+            return None
+        # assigning the key:item value to paraent's instance variable
+        self.cache_data[key] = item
+
+    def get(self, key):
+        """Retrieves a key from the cache"""
+        if key is None or key not in self.cache_data:
+            return None
+        return self.cache_data[key]
+
+    
