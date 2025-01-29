130b84c6f072dc582076e870be873d003e851c1e
egbuniwecemeka <egbuniwecemeka.com>
2025-01-29 02:09:09 +0100

    FIFO caching

--- 0x01-caching/1-fifo_cache.py
+++ 0x01-caching/1-fifo_cache.py
@@ -18,7 +18,7 @@ class FIFOCache(BaseCaching):
         self.cache_data[key] = item
 
         # FIFO Logic
-        if len(self.cache_data) > BaseCaching.MAX_ITEMS:
+        if len(self.cache_data) > BaseCaching.MAX_ITEM:
             first = list(self.cache_data.keys())[0]
             fifo = self.cache_data.pop(first)
             print(f'DISCARD: {first}')
